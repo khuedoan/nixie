98bc72bbfcb93d08142f7bb226677e63450f69dd
Khue Doan <mail@khuedoan.com>
2025-10-10 12:13:47 +0700

    feat(network): add function to broadcast Wake-on-LAN magic packet

--- internal/network/wol.go
+++ internal/network/wol.go
@@ -1,6 +1,9 @@
 package network
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 func buildMacicPacket(mac net.HardwareAddr) []byte {
 	// A physical WakeOnLAN (Magic Packet) will look like this:
@@ -27,4 +30,28 @@ func buildMacicPacket(mac net.HardwareAddr) []byte {
 	}
 
 	return packet
+}
+
+func SendWakeOnLAN(mac net.HardwareAddr) error {
+	// https://superuser.com/questions/295325/does-it-matter-what-udp-port-a-wol-signal-is-sent-to
+	// UDP is recommended because it can be generated without raw sockets which come with security restrictions,
+	// and port 9 is recommended because it maps to the old well-known discard protocol
+	const wolPort = 9
+	magicPacket := buildMacicPacket(mac)
+
+	// TODO no IPv6 for now
+	conn, err := net.DialUDP("udp4", nil, &net.UDPAddr{
+		IP:   net.IPv4bcast,
+		Port: wolPort,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to dial UDP broadcast: %w", err)
+	}
+	defer conn.Close()
+
+	if _, err = conn.Write(magicPacket); err != nil {
+		return fmt.Errorf("failed to send magic packet: %w", err)
+	}
+
+	return nil
 }
