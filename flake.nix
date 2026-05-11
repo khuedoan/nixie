acff83385291a12129d75540f214b3b0967a870c
Khue Doan <mail@khuedoan.com>
2026-05-11 12:05:00 +0700

    chore: upgrade packages

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixie";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
       inputs.nixpkgs.follows = "nixpkgs";
